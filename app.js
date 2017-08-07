a880c9d678c51850309a552f555380658c58a547
KendraB <kendra.l.b87@gmail.com>
2017-08-06 20:09:57 -0600

    commit for debugging

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var map;
 var markers = [];
+var locations = [
+  {title: 'Elitch Gardens', location: {lat: 39.7494, lng: -105.0125}},
+  {title: 'Mile High Stadium', location: {lat: 39.7439, lng: -105.0201}},
+  {title: 'Denver Zoo', location: {lat: 39.7506, lng: -104.9488}},
+  {title: 'Coors Field', location: {lat: 39.7559, lng: -104.9942}},
+  {title: 'Buell Theater', location: {lat: 39.7446, lng: -104.9979}}
+
+];
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -7,17 +15,8 @@ function initMap() {
     zoom: 13
   });
 
-  var locations = [
-    {title: 'Elitch Gardens', location: {lat: 39.7494, lng: -105.0125}},
-    {title: 'Mile High Stadium', location: {lat: 39.7439, lng: -105.0201}},
-    {title: 'Denver Zoo', location: {lat: 39.7506, lng: -104.9488}},
-    {title: 'Coors Field', location: {lat: 39.7559, lng: -104.9942}},
-    {title: 'Buell Theater', location: {lat: 39.7446, lng: -104.9979}}
-
-  ];
-
   var infowindow = new google.maps.InfoWindow({
-    content: 'blah de blah de blah'
+    content: ''
   });
   function populateInfowindow(marker, infowindow) {
     if (infowindow.marker != marker) {
@@ -29,6 +28,8 @@ function initMap() {
       });
     }
   }
+
+  var bounds = new google.maps.LatLngBounds();
 
   for (var i = 0; i < locations.length; i++) {
     //get the position from the location array
@@ -45,19 +46,33 @@ function initMap() {
     // push marker to array of markers
     markers.push(marker);
     //extend boundary of map for each marker
-    //bounds.extend(marker, position);
+    bounds.extend(markers[i].position);
+
     // create onclick event to open infowindow at each marker
     marker.addListener('click', function() {
       populateInfowindow(this, infowindow);
     });
     function toggleBounce() {
+      var marker = this;
       if (marker.getAnimation() !== null) {
         marker.setAnimation(null);
       }
       else {
         marker.setAnimation(google.maps.Animation.BOUNCE);
       }
+      setTimeout(function(){
+        marker.setAnimation(null);
+      }, 750);
     }
     marker.addListener('click', toggleBounce);
+    map.fitBounds(bounds);
   }
 }
+
+var ViewModel = function () {
+  var self = this;
+  self.locations = ko.observableArray(locations);
+
+
+}
+ko.applyBindings(new ViewModel());
